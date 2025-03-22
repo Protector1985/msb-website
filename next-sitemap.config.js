c702de9028442f736f9700d29dace32428380e1c
Protector1985 <michaelrbraun1985@gmail.com>
2025-03-22 13:01:38 -0700

    ...

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,34 @@
 const { default: axios } = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 const fetchDynamicBlogRoutes = async () => {
   const posts = await axios.get(`${process.env.WORDPRESS_API}/posts`);
   return posts.data.map((post) => `/blog/details/${post.id}/${post.slug}`);
+};
+
+const generateSitemapIndex = (numOfSitemaps) => {
+  let sitemapIndexContent = `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+  
+  for (let i = 0; i < numOfSitemaps; i++) {
+    sitemapIndexContent += `  <sitemap>\n    <loc>https://www.msbprotection.com/sitemap-${i}.xml</loc>\n  </sitemap>\n`;
+  }
+  
+  sitemapIndexContent += `</sitemapindex>`;
+  
+  fs.writeFileSync(path.join(__dirname, "public", "sitemap-index.xml"), sitemapIndexContent);
+};
+
+const generateSitemap = (routes, index) => {
+  let sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+  
+  routes.forEach((route) => {
+    sitemapContent += `  <url>\n    <loc>https://www.msbprotection.com${route}</loc>\n    <lastmod>${new Date().toISOString()}</lastmod>\n    <changefreq>daily</changefreq>\n    <priority>0.8</priority>\n  </url>\n`;
+  });
+  
+  sitemapContent += `</urlset>`;
+  
+  fs.writeFileSync(path.join(__dirname, "public", `sitemap-${index}.xml`), sitemapContent);
 };
 
 module.exports = {
@@ -17,12 +43,17 @@ module.exports = {
   ],
   additionalPaths: async (config) => {
     const dynamicBlogRoutes = await fetchDynamicBlogRoutes();
-    return dynamicBlogRoutes.map((route) => ({
-      loc: `${config.siteUrl}${route}`,
-      changefreq: "daily",
-      priority: 0.8,
-      lastmod: new Date().toISOString(),
-    }));
+    const chunkSize = 10;
+    const chunks = [];
+    
+    for (let i = 0; i < dynamicBlogRoutes.length; i += chunkSize) {
+      chunks.push(dynamicBlogRoutes.slice(i, i + chunkSize));
+    }
+
+    chunks.forEach((chunk, index) => generateSitemap(chunk, index));
+    generateSitemapIndex(chunks.length);
+
+    return [];
   },
   transform: async (config, path) => {
     console.log("Processing path:", path);
